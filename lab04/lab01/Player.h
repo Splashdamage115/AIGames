9d3ec5a7c7b243528cd5425b875c49e820c113b3
DavidStrikaitis <davidstrik282@gmail.com>
2025-10-05 11:51:51 +0100

    started work on lab 4

---
+++ lab04/lab01/Player.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Library.h"
+class Player
+{
+public:
+	Player();
+	void start();
+
+	void update();
+
+	sf::Vector2f getPosition() { return m_body->getPosition(); }
+	float getSpeed() { return m_speed; }
+	float getAngle() { return m_angle; }
+private:
+	void outOfBounds();
+
+	void playerInput();
+	void increaseSpeed();
+	void decreaseSpeed();
+	void changeAngle(int t_direction);
+
+	std::shared_ptr<sf::CircleShape>m_body;
+	std::shared_ptr<sf::Sprite> m_sprite;
+	sf::Texture m_texture;
+
+	float m_angle = 0.0f;
+	float m_angleChange = 100.0f;
+
+	float m_speed = 0.0f;
+	float m_maxSpeed = 500.0f;
+	float m_speedIncreaseFactor = 100.0f;
+};
+
---
+++ lab04/lab01/PursueMoveState.cpp
@@ -0,0 +1,48 @@
+#include "PursueMoveState.h"
+#include "Game.h"
+#include "math.h"
+#include "RenderObject.h"
+
+PursueMoveState::PursueMoveState(std::shared_ptr<sf::Vector2f> t_position) : abstractMoveState(t_position)
+{
+}
+
+void PursueMoveState::init()
+{
+    m_seekPoint = std::make_shared<sf::CircleShape>();
+    m_seekPoint->setRadius(5.0f);
+    RenderObject::getInstance().addNewRenderObject(m_seekPoint, 2);
+    m_angle = rand() % 360;
+}
+
+sf::Vector2f PursueMoveState::moveVector(sf::Vector2f t_playerPos, float t_playerAngle, float t_speed)
+{
+    sf::Vector2f predictedPosition = t_playerPos + (math::degreesToDisplacement(t_playerAngle) * lookAheadTime * (t_speed));
+    m_seekPoint->setPosition(predictedPosition);
+    float dist = math::distancebetweenPoints(*m_position, predictedPosition);
+    sf::Vector2f desiredDisplacement;
+
+    if (dist >= arriveDistance) {
+        desiredDisplacement = math::displacement(*m_position, predictedPosition);
+    }
+    else
+    {
+        desiredDisplacement = math::displacement(*m_position, t_playerPos);
+    }
+
+    float desiredAngle = math::displacementToDegrees(desiredDisplacement);
+
+    float change = desiredAngle - m_angle;
+    float angleChange = (static_cast<int>(std::round(change + 360))) % 360;
+
+    if (angleChange < 180)
+    {
+        changeAngle(+1);
+    }
+    else
+    {
+        changeAngle(-1);
+    }
+
+    return math::degreesToDisplacement(m_angle) * Game::deltaTime * m_speed;
+}
