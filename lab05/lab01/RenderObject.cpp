9f3017a9c3bbc83dfa6cd1496093d553ac8a115a
DavidStrikaitis <davidstrik282@gmail.com>
2025-10-30 09:30:58 +0000

    added lab 5 base

---
+++ lab05/lab01/RenderObject.cpp
@@ -0,0 +1,51 @@
+#include "RenderObject.h"
+
+RenderObject::RenderObject() : m_window{ sf::VideoMode{ sf::Vector2u{sf::VideoMode::getDesktopMode().size}, 32U}, "SFML Game 3.0", sf::State::Fullscreen}
+{
+}
+
+void RenderObject::closeWindow()
+{
+	m_window.close();
+}
+
+void RenderObject::start()
+{
+	m_layers.emplace_back();
+}
+
+bool RenderObject::addNewRenderObject(std::shared_ptr<sf::Drawable> t_renderObj, int t_layerNum)
+{
+	if (t_layerNum - 1 > m_layers.size())
+	{
+		while (m_layers.size() < t_layerNum)
+			m_layers.emplace_back();
+	}
+	
+	for (unsigned int i = 0; i < m_layers.at(t_layerNum).size(); i++)
+	{
+		if (m_layers.at(t_layerNum).at(i).lock() == nullptr) // if the object is empty
+		{
+			m_layers.at(t_layerNum).at(i) = t_renderObj;
+			return true;
+		}
+	}
+
+	m_layers.at(t_layerNum).push_back(t_renderObj);
+	return true;
+}
+
+void RenderObject::render()
+{
+	for each(auto layer in m_layers)
+	{
+		if (layer.empty()) continue; // error handling
+
+		for each(auto var in layer)
+		{
+			if (var.lock())
+				m_window.draw(*var.lock());
+		}
+	}
+	m_window.display();
+}
